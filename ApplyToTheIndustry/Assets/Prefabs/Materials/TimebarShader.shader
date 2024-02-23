846341452a444afe9e76c88f970a715fefe23512
amonjerro <eescudero_arens@hotmail.com>
2024-02-22 23:01:57 -0500

    Turned time spent into a bar and made resource and time manager talk to each other

---
+++ ApplyToTheIndustry/Assets/Prefabs/Materials/TimebarShader.shader
@@ -0,0 +1,48 @@
+Shader "Unlit/TimebarShader"
+{
+    Properties
+    {
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = float4(i.uv.y, 0,0,1);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
--- ApplyToTheIndustry/Assets/Scripts/GameResources/ResourceManager.cs
+++ ApplyToTheIndustry/Assets/Scripts/GameResources/ResourceManager.cs
@@ -1,22 +1,32 @@
 using UnityEngine;
 
+// Class in charge of managing the data for resources
+// It sends messages to the TimeManager class as it has some additional time-related
+// behavior that doesn't fit the scope of this class.
+// Should be a child of the ServiceLocator
 public class ResourceManager : MonoBehaviour
 {
-    public static ResourceManager Instance;
+    // Game resources
     private Resource _timeAvailable;
     private Resource _moneyAvailable;
 
+    // Configurable parameters that set the limit of the game;
     [SerializeField]
     public Resource maxMoney;
     [SerializeField]
     public Resource maxTime;
 
+    // Support function - if we ever want to bind the maximums players can reset to,
+    // this would be the way to do it.
     public void ReduceMaximums(Resource time, Resource money)
     {
         maxTime -= time;
+        ServiceLocator.Instance.GetService<TimeManager>().SetMaxTime(maxTime.value);
         maxMoney -= money;
     }
 
+    // Test function for UI and other processes that need to know whether
+    // a cost can be met with the available resources
     public bool IsCostViable(Cost cost)
     {
         bool enoughTime = _timeAvailable.IsThereEnough(cost.time.value);
@@ -24,15 +34,19 @@ public class ResourceManager : MonoBehaviour
         return enoughMoney && enoughTime;
     }
 
+    // Handle expending some cost
     public void ManageCost(Cost cost)
-        {
+    {
         _timeAvailable -= cost.time;
+        ServiceLocator.Instance.GetService<TimeManager>().ExpendTime(cost.time.value);
         _moneyAvailable -= cost.money;
     }
 
+    // Reset the status of resources
     public void ResetResources()
     {
         _timeAvailable = maxTime;
+        ServiceLocator.Instance.GetService<TimeManager>().ResetTimer();
         _moneyAvailable = maxMoney;
     }
 }
