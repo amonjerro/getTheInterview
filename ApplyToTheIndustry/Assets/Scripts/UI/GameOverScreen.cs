e1bc01efb1ef25561e31402c5057dc0d54ab93fd
Robert Reddick (RIT Student) <rdr8959@rit.edu>
2024-02-29 14:45:10 -0500

    Added comments to code

--- ApplyToTheIndustry/Assets/Scripts/UI/GameOverScreen.cs
+++ ApplyToTheIndustry/Assets/Scripts/UI/GameOverScreen.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/* CLASS: GameOverScreenBehavior
+ * Used for defining button behaviors for
+ * game over screen
+ */
 public class GameOverScreenBehavior : MonoBehaviour
 {
+    /// <summary>
+    /// Restarts the main game scene
+    /// </summary>
     public void RestartGame()
     {
         SceneManager.LoadScene(1);
     }
 
+    /// <summary>
+    /// Sends player back to main menu
+    /// </summary>
     public void MainMenu()
     {
         SceneManager.LoadScene(0);
     }
 
+    /// <summary>
+    /// Quits the game app
+    /// </summary>
     public void QuitGame()
     {
         Application.Quit();
